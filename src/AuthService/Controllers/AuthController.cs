d68c84651df3e7421c270136f4f5282646760524
Daniel R. <46886439+DanielRudCallesen@users.noreply.github.com>
2025-12-11 14:05:51 +0100

    Feature/docker image refactor (#25)
    * Add CodeQL analysis workflow configuration (#21)
    
    * Test for docker image
    
    * Docker images changes

--- src/AuthService/Controllers/AuthController.cs
+++ src/AuthService/Controllers/AuthController.cs
@@ -12,7 +12,6 @@ namespace AuthService.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IConfiguration _configuration;
-
         public AuthController(IConfiguration configuration)
         {
             _configuration = configuration;
