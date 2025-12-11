8c717d51fdc19084310ea87ee8237f68d242e355
Daniel R. <46886439+DanielRudCallesen@users.noreply.github.com>
2025-12-11 09:54:32 +0100

    Developer branch to release on main  (#13)
    * test: verify branch protection
    
    * fd (#9)
    
    * Feature/auth service setup (#4)
    
    * test: verify branch protection
    
    * test2: verify branch protection
    
    * Repo setup
    
    * Cleaned up and setup
    
    * Deleted useless items
    
    * ci: add initial CI workflows for branch protection
    
    * Updated path (#7)
    
    * Feature/setup (#8)
    
    * Updated path
    
    * Updated path
    
    * Feature/setup (#10)
    
    * Feature/auth service setup (#4)
    
    * test: verify branch protection
    
    * test2: verify branch protection
    
    * Repo setup
    
    * Cleaned up and setup
    
    * Deleted useless items
    
    * ci: add initial CI workflows for branch protection
    
    * Updated path
    
    * Updated path (#7)
    
    * Updated path
    
    * .slnx for newer version of visual studio
    
    * ci: implement complete CI/CD pipelines for all services
    
    * ci: add CodeQL, Depndabot
    
    * Create codeql-config.yml
    
    * Changed Services to Src
    
    * Feature/setup (#12)
    
    * Feature/auth service setup (#4)
    
    * test: verify branch protection
    
    * test2: verify branch protection
    
    * Repo setup
    
    * Cleaned up and setup
    
    * Deleted useless items
    
    * ci: add initial CI workflows for branch protection
    
    * Updated path
    
    * Updated path (#7)
    
    * Updated path
    
    * .slnx for newer version of visual studio
    
    * ci: implement complete CI/CD pipelines for all services
    
    * ci: add CodeQL, Depndabot
    
    * Create codeql-config.yml
    
    * Changed Services to Src
    
    * Fix whitespace formatting

---
+++ src/AuthService/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        
+    }
+}
---
+++ src/GatewayService/Controllers/GatewayController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GatewayService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GatewayController : ControllerBase
+    {
+        
+    }
+}
