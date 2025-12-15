caf98189510ac52c435b7a189402d8c0d6d023fd
Daniel R. <danielrudcallesen96@gmail.com>
2025-12-15 09:59:19 +0100

    Changes

--- tests/AuthService.Tests/AuthControllerTests.cs
+++ tests/AuthService.Tests/AuthControllerTests.cs
@@ -5,13 +5,28 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Xunit;
+
 public class AuthControllerTests
 {
+    private IConfiguration CreateTestConfiguration()
+    {
+        var inMemorySettings = new Dictionary<string, string?>
+        {
+            {"Jwt:Key", "TestSecretKeyThatIsLongEnoughForHS256Algorithm12345"},
+            {"Jwt:Issuer", "TestAuthService"},
+            {"Jwt:Audience", "TestMicroservicesApp"}
+        };
+
+        return new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+    }
+
     [Fact]
     public void Health_ReturnsOkResult()
     {
         // Arrange
-        var config = new ConfigurationBuilder().Build();
+        var config = CreateTestConfiguration();
         var controller = new AuthController(config);
 
         // Act
@@ -25,7 +40,7 @@ public class AuthControllerTests
     public void GenerateToken_WithValidUsername_ReturnsToken()
     {
         // Arrange
-        var config = new ConfigurationBuilder().Build();
+        var config = CreateTestConfiguration();
         var controller = new AuthController(config);
         var request = new TokenRequest("testuser");
 
@@ -35,13 +50,20 @@ public class AuthControllerTests
         // Assert
         result.Should().NotBeNull();
         result!.Value.Should().NotBeNull();
+        
+        // Verify the response structure
+        var response = result.Value as TokenResponse;
+        response.Should().NotBeNull();
+        response!.Token.Should().NotBeNullOrEmpty();
+        response.Username.Should().Be("testuser");
+        response.ExpiresAt.Should().BeAfter(DateTime.UtcNow);
     }
 
     [Fact]
     public void GenerateToken_WithEmptyUserName_ReturnsBadRequest()
     {
         // Arrange
-        var config = new ConfigurationBuilder().Build();
+        var config = CreateTestConfiguration();
         var controller = new AuthController(config);
         var request = new TokenRequest("");
 
@@ -50,5 +72,25 @@ public class AuthControllerTests
 
         // Assert
         result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Fact]
+    public void GenerateToken_CreatesValidJwtToken()
+    {
+        // Arrange
+        var config = CreateTestConfiguration();
+        var controller = new AuthController(config);
+        var request = new TokenRequest("testuser");
+
+        // Act
+        var result = controller.GenerateToken(request) as OkObjectResult;
+        var response = result!.Value as TokenResponse;
+
+        // Assert
+        response!.Token.Should().NotBeNullOrEmpty();
+        
+        // JWT tokens have 3 parts separated by dots
+        var tokenParts = response.Token.Split('.');
+        tokenParts.Should().HaveCount(3, "JWT tokens consist of header.payload.signature");
     }
 }
